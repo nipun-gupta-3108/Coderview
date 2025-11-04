c564d2f5c0114c567b1014c296fccef6629f165b
nipun-gupta-3108 <nipungupta082006@gmail.com>
2025-11-05 04:50:28 +0530

    db and tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 
@@ -24,4 +25,15 @@ app.get("/{*any}", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
 });
 
-app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () =>
+      console.log("Server is running on port:", ENV.PORT)
+    );
+  } catch (error) {
+    console.log("Error starting the server", error);
+  }
+};
+
+startServer();
