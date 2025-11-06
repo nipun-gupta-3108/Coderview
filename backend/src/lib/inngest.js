8f4a4fd608813b11bae5977e6f6203e6f18777e9
nipun-gupta-3108 <nipungupta082006@gmail.com>
2025-11-07 01:09:38 +0530

    auth & stream sync

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
+import { upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({ id: "Coderview" });
 
@@ -23,7 +24,11 @@ const syncUser = inngest.createFunction(
 
     await User.create(newUser);
 
-    // todo: do sth else
+    await upsertStreamUser({
+      id: newUser.clerkId.toString(),
+      name: newUser.name,
+      image: newUser.profileImage,
+    });
   }
 );
 
@@ -36,7 +41,7 @@ const deleteUserFromDB = inngest.createFunction(
     const { id } = event.data;
     await User.deleteOne({ clerkId: id });
 
-    // todo: do something else
+    await deleteStreamUser(id.toString());
   }
 );
 
